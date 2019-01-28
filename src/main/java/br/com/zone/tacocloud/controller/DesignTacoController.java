3c2f77f3faf4880f08e4a24333e3b7d60e5c43a1
Daniel Tavares <daniel.tavares@ufca.edu.br>
2019-01-28 13:28:34 -0300

    Adding form validation

--- src/main/java/br/com/zone/tacocloud/controller/DesignTacoController.java
+++ src/main/java/br/com/zone/tacocloud/controller/DesignTacoController.java
@@ -8,10 +8,12 @@ import br.com.zone.tacocloud.model.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -49,7 +51,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco taco){
+    public String processDesign(@Valid Taco taco, Errors errors){
+
+        if(errors.hasErrors()){
+            return "design";
+        }
+
         log.info("Taco " +taco);
         return "redirect:/orders/current";
     }
--- src/main/java/br/com/zone/tacocloud/model/Taco.java
+++ src/main/java/br/com/zone/tacocloud/model/Taco.java
@@ -2,12 +2,18 @@ package br.com.zone.tacocloud.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
 
+    @NotBlank(message="Name is required")
     private String name;
+
+    @Size(min=1, message="You must choose at least 1 ingredient")
     private List<String> ingredients;
 
 }
